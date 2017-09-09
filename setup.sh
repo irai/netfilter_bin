76e41e796ff56f421698b7f73f03401ac2fb282e
FB <irai852@gmail.com>
2017-09-09 01:56:14 +0000

    v0.3

---
+++ setup.sh
@@ -0,0 +1,5 @@
+NETFILTER_DIR=/home/pi/spinifex/golang/bin
+VERSION=0.3
+
+sudo ln -s ./${VERSION}/netfilter ${NETFILTER_DIR}/netfilter
+sudo ln -s ./${VERSION}/netfilter.systemd ${NETFILTER_DIR}/netfilter.systemd
