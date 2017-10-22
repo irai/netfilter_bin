ccdc7d1052c603e077ee7e1e71a19b68d1d12f3e
FB <irai852@gmail.com>
2017-10-22 00:30:08 +0000

    added firewall.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ VERSION=v0.4
 
 BINARIES=${NETFILTER_DIR}/netfilter_bin/${VERSION}
 
-rm ${NETFILTER_DIR}/netfilter ${NETFILTER_DIR}/netfilter.systemd
+sudo rm ${NETFILTER_DIR}/netfilter 
+sudo rm ${NETFILTER_DIR}/netfilter.systemd
+sudo rm ${NETFILTER_DIR}/firewall.sh
 
 sudo ln -s ${BINARIES}/netfilter ${NETFILTER_DIR}/netfilter
 sudo ln -s ${BINARIES}/netfilter.systemd ${NETFILTER_DIR}/netfilter.systemd
+sudo ln -s ${BINARIES}/firewall.sh ${NETFILTER_DIR}/firewall.sh
