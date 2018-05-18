ebdae19e3c309133e792644dcabefc52e389984f
FB <irai852@gmail.com>
2018-05-18 04:50:29 +0000

    fixes

--- setup.sh
+++ setup.sh
@@ -7,11 +7,15 @@ BINARIES=${NETFILTER_DIR}/netfilter_bin/${VERSION}
 # create new symbolic links
 #
 sudo rm ${NETFILTER_DIR}/netfilter 
-sudo rm ${NETFILTER_DIR}/netfilter.systemd
-sudo rm ${NETFILTER_DIR}/firewall.sh
-
 sudo ln -s ${BINARIES}/netfilter ${NETFILTER_DIR}/netfilter
-sudo ln -s ${BINARIES}/netfilter.systemd ${NETFILTER_DIR}/netfilter.systemd
+
+sudo rm ${NETFILTER_DIR}/netfilter.script
+sudo ln -s ${BINARIES}/netfilter.script ${NETFILTER_DIR}/netfilter.script
+
+sudo rm ${NETFILTER_DIR}/download.script
+sudo ln -s ${BINARIES}/download.script ${NETFILTER_DIR}/download.script
+
+sudo rm ${NETFILTER_DIR}/firewall.sh
 sudo ln -s ${BINARIES}/firewall.sh ${NETFILTER_DIR}/firewall.sh
 
 
@@ -21,5 +25,9 @@ SYSTEMD_DIR=/etc/systemd/system
 
 sudo rm ${SYSTEMD_DIR}/netfilter.service
 sudo ln -s ${BINARIES}/netfilter.service ${SYSTEMD_DIR}/netfilter.service
+
+sudo rm ${SYSTEMD_DIR}/download.service
+sudo ln -s ${BINARIES}/download.service ${SYSTEMD_DIR}/download.service
+
 sudo /bin/systemctl daemon-reload
 
