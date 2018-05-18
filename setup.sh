eaa1e02add99fbaf40f9c6f189354ec4d34fd5d1
FB <irai852@gmail.com>
2018-05-18 04:01:50 +0000

    v0.11 with new service logic & restart

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ VERSION=v0.11
 
 BINARIES=${NETFILTER_DIR}/netfilter_bin/${VERSION}
 
+# create new symbolic links
+#
 sudo rm ${NETFILTER_DIR}/netfilter 
 sudo rm ${NETFILTER_DIR}/netfilter.systemd
 sudo rm ${NETFILTER_DIR}/firewall.sh
@@ -11,3 +13,13 @@ sudo rm ${NETFILTER_DIR}/firewall.sh
 sudo ln -s ${BINARIES}/netfilter ${NETFILTER_DIR}/netfilter
 sudo ln -s ${BINARIES}/netfilter.systemd ${NETFILTER_DIR}/netfilter.systemd
 sudo ln -s ${BINARIES}/firewall.sh ${NETFILTER_DIR}/firewall.sh
+
+
+# setup systemd services
+#
+SYSTEMD_DIR=/etc/systemd/system
+
+sudo rm ${SYSTEMD_DIR}/netfilter.service
+sudo ln -s ${BINARIES}/netfilter.service ${SYSTEMD_DIR}/netfilter.service
+sudo /bin/systemctl daemon-reload
+
