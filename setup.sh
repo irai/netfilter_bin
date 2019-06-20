ff8d30a66330f5333d3bd2593725f694150d119d
Fabio Bastian <irai852@gmail.com>
2019-06-20 22:32:42 +0100

    added dynamic syslog mac address

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
-NETFILTER_DIR=/home/pi/spinifex/bin
+NETFILTER_BIN=/home/pi/spinifex/bin
 VERSION=v0.20
          
+LATEST=${NETFILTER_BIN}/netfilter_bin/${VERSION}
 
-BINARIES=${NETFILTER_DIR}/netfilter_bin/${VERSION}
+
 
 # create new symbolic links
 #
-sudo rm ${NETFILTER_DIR}/netfilter 
-sudo ln -s ${BINARIES}/netfilter ${NETFILTER_DIR}/netfilter
+sudo rm ${NETFILTER_BIN}/netfilter 
+sudo ln -s ${LATEST}/netfilter ${NETFILTER_BIN}/netfilter
 
-sudo rm ${NETFILTER_DIR}/netfilter.script
-sudo ln -s ${BINARIES}/netfilter.script ${NETFILTER_DIR}/netfilter.script
+sudo rm ${NETFILTER_BIN}/netfilter.script
+sudo ln -s ${LATEST}/netfilter.script ${NETFILTER_BIN}/netfilter.script
 
-sudo rm ${NETFILTER_DIR}/download.script
-sudo ln -s ${BINARIES}/download.script ${NETFILTER_DIR}/download.script
+sudo rm ${NETFILTER_BIN}/download.script
+sudo ln -s ${LATEST}/download.script ${NETFILTER_BIN}/download.script
 
-sudo rm ${NETFILTER_DIR}/firewall.sh
-sudo ln -s ${BINARIES}/firewall.sh ${NETFILTER_DIR}/firewall.sh
+sudo rm ${NETFILTER_BIN}/firewall.sh
+sudo ln -s ${LATEST}/firewall.sh ${NETFILTER_BIN}/firewall.sh
 
 
 # setup systemd services
@@ -24,17 +25,24 @@ sudo ln -s ${BINARIES}/firewall.sh ${NETFILTER_DIR}/firewall.sh
 SYSTEMD_DIR=/etc/systemd/system
 
 sudo rm ${SYSTEMD_DIR}/netfilter.service
-sudo ln ${BINARIES}/netfilter.service ${SYSTEMD_DIR}/netfilter.service
+sudo ln ${LATEST}/netfilter.service ${SYSTEMD_DIR}/netfilter.service
 
 sudo rm ${SYSTEMD_DIR}/download.service
-sudo ln ${BINARIES}/download.service ${SYSTEMD_DIR}/download.service
+sudo ln ${LATEST}/download.service ${SYSTEMD_DIR}/download.service
 sudo rm ${SYSTEMD_DIR}/download.timer
-sudo ln ${BINARIES}/download.timer ${SYSTEMD_DIR}/download.timer
+sudo ln ${LATEST}/download.timer ${SYSTEMD_DIR}/download.timer
+
+# update syslogd with unique mac
+mac=`ifconfig -a eth0 | awk '/ether/ { print $2 } '`
+sudo cat ${LATEST}/22-loggly.conf | sed 's/MAC_ADDRESS/'$mac'/g' > /etc/rsyslog.d/22-loggly.conf
 
 sudo /bin/systemctl daemon-reload
 
+sudo systemctl enable rsyslog.service
 sudo systemctl enable download.timer
 sudo systemctl enable download.service
 sudo systemctl enable netfilter.service
+sudo systemctl restart rsyslog.service
 sudo systemctl restart netfilter.service
 sudo systemctl restart download.timer
+
